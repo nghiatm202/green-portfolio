97fd8464cedfa0e10d303bbfb919f085e0e35c35
Tran Minh Nghia <nghiatm202@gmail.com>
2022-01-28 12:23:42 +0700

    second commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ const navbarIcons = document.querySelector(".navbar__icons");
 
 const navbarLinks = document.querySelector(".navbar__links");
 
+const overlay = document.querySelector("#overlay");
+
 navbarIcons.onclick = function () {
   navbarLinks.classList.toggle("navbar__open");
   navbarIcons.classList.toggle("open");
+  overlay.classList.toggle("show");
+};
+
+overlay.onclick = function () {
+  navbarLinks.classList.toggle("navbar__open");
+  navbarIcons.classList.toggle("open");
+  overlay.classList.toggle("show");
 };
